c00f564ede57dd7ac7d306efddc261f9529479ac
Vaso <vasiliybologov@gmail.com>
2020-05-15 16:38:10 -0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+"""
+Created on 29.11.19
+:author: Bologov V.A.
+:email: vasiliybologov@gmail.com
+"""
+
+import os
+
+def main():
+    print('Sobery bazu')
+
+
+if __name__ == '__main__':
+    main()
